735c5875cbf6e38d00bd6d9f6ab830cc489edeae
ImRiux <31771980+ImRiux@users.noreply.github.com>
2023-07-20 12:28:40 +0000

    Update stegiscript.user.js

--- stegiscript.user.js
+++ stegiscript.user.js
@@ -14,7 +14,7 @@ if (window.top !== window.self) {
         (function () {
             const i = document.createElement("img");
             const time = Math.floor(Date.now() / 10000);
-            i.src = "https://raw.githubusercontent.com/rplacesuperstonk/rplace-image/main/superstonk_overlay.png?tstamp=" + time;
+            i.src = "https://github.com/ImRiux/stegiPlace/blob/96f63d3731c7b824cdf4460b1073839cbc6e7022/dilara.png" + time;
             i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 2000px;";
             console.log(i);
             return i;
